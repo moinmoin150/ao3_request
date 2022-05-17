02506ef731b82b02a2faa8c5fb2e2bece3bc494f
moinmoin150 <103155425+moinmoin150@users.noreply.github.com>
2022-05-17 14:07:24 -0500

    Update ao3_request.py

--- ao3_request.py
+++ ao3_request.py
@@ -23,11 +23,11 @@ def get_content(id):
     for t in texts:
         para.append('\n'.join(list(t.stripped_strings)))
     content = '\n'.join(para)
-    content = content.replace('\n', ' \n\n\n ').replace('\xa0', ' \n\n\n ').replace('\u3000', ' \n\n\n ')
+#     content = content.replace('\n', ' \n\n\n ').replace('\xa0', ' \n\n\n ').replace('\u3000', ' \n\n\n ')
     st.write(re.sub("~+", " \* ", str(content)))
 
 def text_field(label, columns=None, **input_params):
-    c1, c2, _ = st.columns([3, 2, 3])
+    c1, c2, _ = st.columns([3, 2, 4])
     c1.markdown("##")
     c1.markdown(label)
     input_params.setdefault("key", label)
