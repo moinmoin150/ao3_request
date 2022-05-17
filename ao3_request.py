3d27f0870c2f817a449190221736c377a4d0030b
moinmoin150 <103155425+moinmoin150@users.noreply.github.com>
2022-05-17 14:02:58 -0500

    Update ao3_request.py

--- ao3_request.py
+++ ao3_request.py
@@ -27,7 +27,7 @@ def get_content(id):
     st.write(re.sub("~+", " \* ", str(content)))
 
 def text_field(label, columns=None, **input_params):
-    c1, c2 = st.columns(columns or [4, 1])
+    c1, c2, _ = st.columns([2, 1, 3])
     c1.markdown("##")
     c1.markdown(label)
     input_params.setdefault("key", label)
