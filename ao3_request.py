d8d4dcd33c91ca9d8b762a7567557c75026faf72
moinmoin150 <103155425+moinmoin150@users.noreply.github.com>
2022-05-17 13:57:12 -0500

    Add files via upload

---
+++ ao3_request.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import re
+from bs4 import BeautifulSoup, NavigableString
+import urllib.request
+
+def navigate_chapters(work_id):
+    url = 'https://archiveofourown.org/works/' + str(work_id) + '/navigate?view_adult=true'
+    req = urllib.request.Request(url)
+    resp = urllib.request.urlopen(req)
+    bs = BeautifulSoup(resp, 'lxml')
+    chapters = bs.find('ol', {'class':'chapter index group'}).find_all('li')
+    links = [i.find('a').get('href') for i in chapters]
+    return links
+
+def get_content(id):
+    url = 'https://archiveofourown.org' + str(id) + '?view_adult=true'
+    req = urllib.request.Request(url)
+    resp = urllib.request.urlopen(req)
+    bs = BeautifulSoup(resp, 'lxml')
+    chapters = bs.find('div', {'id':'chapters'})
+    texts = chapters.find_all('p')
+    para = []
+    for t in texts:
+        para.append('\n'.join(list(t.stripped_strings)))
+    content = '\n'.join(para)
+    content = content.replace('\n', ' \n\n\n ').replace('\xa0', ' \n\n\n ').replace('\u3000', ' \n\n\n ')
+    st.write(re.sub("~+", " \* ", str(content)))
+
+def text_field(label, columns=None, **input_params):
+    c1, c2 = st.beta_columns(columns or [1, 4])
+    c1.markdown("##")
+    c1.markdown(label)
+    input_params.setdefault("key", label)
+    return c2.text_input("", **input_params)
+
+
+
+st.markdown("# 给我一篇FanFic！")
+st.text("给我一个数字ID")
+work_id = text_field("archiveofourown.org/works/")
+links = navigate_chapters(work_id)
+options = [f'Chapter {i+1}' for i in range(len(links))]
+if len(links) == 1:
+    get_content(f'/works/{work_id}')
+else:
+    option = st.selectbox('How would you like to be contacted?', options)
+    get_content(links[options.index(option)])
