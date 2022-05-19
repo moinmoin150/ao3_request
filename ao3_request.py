ded35c047a5c36ebceaa99e10fe2ab911b300433
moinmoin150 <103155425+moinmoin150@users.noreply.github.com>
2022-05-19 18:52:43 -0500

    Update ao3_request.py

--- ao3_request.py
+++ ao3_request.py
@@ -2,6 +2,32 @@ import streamlit as st
 import re
 from bs4 import BeautifulSoup, NavigableString
 import urllib.request
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+
+data= pd.read_csv('GGAD_test.csv', index_col=0) 
+
+gb = GridOptionsBuilder.from_dataframe(data)
+gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+gb.configure_side_bar() #Add a sidebar
+gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+gridOptions = gb.build()
+
+grid_response = AgGrid(
+    data,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='MODEL_CHANGED', 
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=True,
+    height=350, 
+    width='100%',
+    reload_data=True
+)
+
+data = grid_response['data']
+selected = grid_response['selected_rows'] 
+df = pd.DataFrame(selected)
 
 def navigate_chapters(work_id):
     url = 'https://archiveofourown.org/works/' + str(work_id) + '/navigate?view_adult=true'
