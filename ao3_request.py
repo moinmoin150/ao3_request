cc6056c24d2e6a77decb7fb5eac72b87109d9ab5
moinmoin150 <103155425+moinmoin150@users.noreply.github.com>
2022-05-19 22:35:48 -0500

    Update ao3_request.py

--- ao3_request.py
+++ ao3_request.py
@@ -93,7 +93,7 @@ def scrape():
 def display(data):
     gb = GridOptionsBuilder.from_dataframe(data)
     gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
-    gb.configure_default_column({wrapText:True, autoHeight:True})
+    gb.configure_default_column({"wrapText":True, "autoHeight":True})
     gb.configure_side_bar() #Add a sidebar
 #     gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
     gridOptions = gb.build()
